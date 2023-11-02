6285dfdabecdf28881aecb7568849ac4c0bc9eb3
samuel-ferreira-85 <sam85avila@gmail.com>
2023-11-02 20:54:14 -0300

    Mais conversões e obtenção de dados

---
+++ DataHora/src/app/App.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class App {
+
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.parse("2023-11-02");
+        LocalDateTime localDateTime = LocalDateTime.parse("2023-11-02T16:58:00");
+        Instant instant = Instant.parse("2023-11-02T01:30:00Z");
+
+        LocalDate r1 = LocalDate.ofInstant(instant, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(instant, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(instant, ZoneId.of("Portugal"));
+
+        System.out.println("R1 -> " + r1);
+        System.out.println("R2 -> " + r2);
+        System.out.println("R3 -> " + r3);
+        System.out.println("R4 -> " + r4);
+        System.out.println("-- Obtendo dados de uma data local --");
+        System.out.println("Dia = " + localDate.getDayOfMonth());
+        System.out.println("Mês = " + localDate.getMonth());
+        System.out.println("Mês = " + localDate.getMonthValue());
+        System.out.println("Ano = " + localDate.getYear());
+        System.out.println("Horas = " + localDateTime.getHour());
+        System.out.println("Minutoa = " + localDateTime.getMinute());
+        System.out.println("Segundos = " + localDateTime.getSecond());
+
+    }
+
+}
---
+++ DataHora/src/main/Programa.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Programa {
+
+    public static void main(String[] args) {
+        // Instanciação do agora
+        LocalDate agoraLocalDate = LocalDate.now();
+        LocalDateTime agoraLocalDateTime = LocalDateTime.now();
+        Instant agoraInstant = Instant.now();
+
+        // Conversão de string para classes time
+        LocalDate strToLocalDate = LocalDate.parse("2023-11-02");
+        LocalDateTime strToLocalDateTime = LocalDateTime.parse("2023-11-02T16:58:00");
+        Instant instant = Instant.parse("2023-11-02T16:58:00Z");
+        Instant strToInstantLocal = Instant.parse("2023-11-02T16:58:00-03:00");
+
+        LocalDate formato1 = LocalDate.parse("27/08/1985", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalDateTime formato2 = LocalDateTime.parse("27/08/1985 08:30", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+
+        LocalDate data = LocalDate.of(2023, 11, 2);
+        LocalDateTime dataHora = LocalDateTime.of(1985, 8, 27, 8, 30);
+
+        // Criando formatos
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+
+        System.out.println("--- Instanciação ---");
+        System.out.println("Agora LocalDate: " + agoraLocalDate);
+        System.out.println("Agora LocalDateTime: " + agoraLocalDateTime);
+        System.out.println("Agora Instant: " + agoraInstant);
+        System.out.println("--- Conversão ---");
+        System.out.println("Para LocalDate: " + strToLocalDate);
+        System.out.println("Para LocalDateTime: " + strToLocalDateTime);
+        System.out.println("Para Instant: " + instant);
+        System.out.println("Para Instant Local: " + strToInstantLocal);
+        System.out.println("Para LocalDate 00/00/0000: " + formato1);
+        System.out.println("Para LocalDate 00/00/0000 00:00:00 : " + formato2);
+        System.out.println("Criando data : " + data);
+        System.out.println("Criando dataHora : " + dataHora);
+        System.out.println("Formato 1: " + data.format(fmt1));
+        System.out.println("Formato 2: " + fmt1.format(data));
+        System.out.println("Formato 3: " + data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        System.out.println("Formato 4: " + dataHora.format(fmt2));
+        System.out.println("Formato 4: " + dataHora.format(fmt1));
+        System.out.println("Formato 5: " + fmt3.format(agoraInstant));
+
+
+    }
+}
